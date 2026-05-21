bfa30845ba53c2be91fd85aeb930502a2d679cdf
M.Atif63 <muhammed.atif.ali.63@gmail.com>
2026-05-21 18:27:37 +0500

    refactor: simplify Button component and update App to include Home

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,12 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
+import Home from './assets/components/Pages/Home'
 
 function App() {
-  const [count, setCount] = useState(0)
   return (
     <>
-    
+    <Home/>
     </>
   )
 }
