42fe0454c7422e193830154d6a4bb8e5690d7125
M.Atif63 <muhammed.atif.ali.63@gmail.com>
2026-05-21 18:32:58 +0500

    feat:  implement routing in Navbar component

--- src/assets/components/Navbar/Navbar.jsx
+++ src/assets/components/Navbar/Navbar.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Home from '../Pages/Home'
+import About from '../Pages/About'
+import Contact from '../Pages/Contact'
+import Feedback from '../Pages/Feedback'
+import Blogs from '../Pages/Blogs'
+import Pricing from '../Pages/Pricing'
 
 function Navbar() {
   return (
     <div>
-      
+        <BrowserRouter>
+        <Routes>
+            <Route path='/' element={<Home/>}/>
+            <Route path='/about' element={<About/>}/>
+            <Route path='/feedback' element={<Feedback/>}/>
+            <Route path='/contact' element={<Contact/>}/>
+            <Route path='/blogs' element={<Blogs/>}/>
+            <Route path='/pricing' element={<Pricing/>}/>
+        </Routes>
+        </BrowserRouter>
     </div>
   )
 }
